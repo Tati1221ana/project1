195c16130849307274bba359d7953ae49dd36578
Tatiana <tpuzova1997@gmail.com>
2021-11-04 12:49:29 +0300

    exercise with modificators

---
+++ src/restaurant/Guests.java
@@ -0,0 +1,19 @@
+package restaurant;
+
+public class Guests extends Restaurant {
+
+    @Override
+    void money() {
+
+    }
+
+    @Override
+    void place() {
+
+    }
+
+    @Override
+    void orders() {
+
+    }
+}
---
+++ src/restaurant/VipGuests.java
@@ -0,0 +1,22 @@
+package restaurant;
+
+public class VipGuests extends Restaurant {
+    @Override
+    void money() {
+
+    }
+
+    @Override
+    void place() {
+
+    }
+
+    @Override
+    void orders() {
+
+    }
+
+    private void dance(){
+
+    }
+}
